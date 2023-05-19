90606c36eb34494f03a48193ad73f4afe42cc148
gkstkdgkr <gkstkdgkr12@gmail.com>
2023-05-19 12:55:33 +0900

    230519 study

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Wetube Reloaded
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/edit -> Edit Profile
+/users/delete -> Delete Profile
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comment -> Comment on a video
+/videos/comment/delete -> Delete A Comment of a video
---
+++ src/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
+const PORT = 4000
+
+const app = express();
+const logger = morgan("dev");
+app.use(logger)
+
+
+app.use("/",globalRouter);
+app.use("/videos",videoRouter);
+app.use("/users",userRouter);
+
+
+// const home = (req, res)=> {
+//   console.log("I will respond.")
+//   return res.send("www.naver.com");
+// }
+// const login = (req,res)=>{
+//   return res.send("login")
+// }
+
+const handleListening = () => console.log(`Server listening on port localhost:${PORT}✨`);
+
+app.listen(PORT, handleListening) // port_numberb
