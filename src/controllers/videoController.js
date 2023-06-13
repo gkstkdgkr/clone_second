9edee983d6ba0a86f99188baf16d5bc31b9003cc
gkstkdgkr <gkstkdgkr12@gmail.com>
2023-06-13 17:33:42 +0900

    230613 study_2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -40,7 +40,30 @@ export const getEdit = (req, res) => {
   const video = videos[id - 1];
   return res.render("edit",{pageTitle:`Editing : ${video.title}`,video})
 };
+
 export const postEdit = (req, res) => {
-  return res.redirect();
+  // const id = req.params.id
+  const {id} = req.params;
+  const {title} =req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`); 
 };
 
+export const getUpload = (req, res) => { 
+  return res.render("upload",{pageTitle : "Upload Video"});
+};
+
+// req.body를 통해 input에 있는 내용을 받아올 수 있음
+export const postUpload = (req, res) => {
+  const {title} = req.body;
+  const newVideo = {
+    title,
+    rating : 0,
+    comments : 0,
+    createdAt : "Just now",
+    view : 0,
+    id : videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
+};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,5 +1,13 @@
 import express from "express";
-import { watch, postEdit, getEdit, upload, deleteVideo } from "../controllers/videoController";
+import { 
+  watch, 
+  postEdit, 
+  getEdit, 
+  upload, 
+  deleteVideo, 
+  getUpload,
+  postUpload,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
@@ -7,7 +15,9 @@ const videoRouter = express.Router();
 // :id가 위에있으면 /upload는 인식 못함
 // 리퀘스트는 제일 위에있는것부터 순차적으로 확인함
 
+// :id(//d+) id를 불러오는데 숫자만 받아옴 
+// :id는 express에게 id라는 변수를 받아오라는 표시
 videoRouter.get("/:id(\\d+)",watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
-
+videoRouter.route("/upload").get(getUpload).post(postUpload)
 export default videoRouter;
