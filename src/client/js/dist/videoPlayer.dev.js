56756bb97527751690789fdb12877bb656f425ea
gkstkdgkr <gkstkdgkr12@gmail.com>
2023-08-16 13:04:26 +0900

    study 230816
    video controller

--- src/client/js/dist/videoPlayer.dev.js
+++ src/client/js/dist/videoPlayer.dev.js
@@ -3,8 +3,12 @@
 var video = document.querySelector("video");
 var playBtn = document.getElementById("play");
 var muteBtn = document.getElementById("mute");
-var time = document.getElementById("time");
-var volume = document.getElementById("volume");
+var volumeRange = document.getElementById("volume");
+var currentTime = document.getElementById("currentTime");
+var totalTime = document.getElementById("totalTime");
+var timeline = document.getElementById("timeline");
+var volumeValue = 0.5;
+video.volume = volumeValue;
 
 var handlePlayClick = function handlePlayClick(e) {
   if (video.paused) {
@@ -12,19 +16,61 @@ var handlePlayClick = function handlePlayClick(e) {
   } else {
     video.pause();
   }
+
+  playBtn.innerText = video.paused ? "Pause" : "Play";
 };
 
-var handlePause = function handlePause() {
-  return playBtn.innerText = "Play";
+var handleMuteClick = function handleMuteClick(e) {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
-var handlePlay = function handlePlay() {
-  return playBtn.innerText = "Pause";
+var handleVolumeChange = function handleVolumeChange(e) {
+  var value = e.target.value;
+
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+
+  volumeValue = value;
+  video.volume = value;
+
+  if (video.volume === 0) {
+    muteBtn.innerText = "Unmute";
+  } else {
+    muteBtn.innerText = "Mute";
+  }
 };
 
-var handleMute = function handleMute(e) {};
+var formatTime = function formatTime(seconds) {
+  return new Date(seconds * 1000).toISOString().substr(14, 5);
+};
+
+var handleLoadedMetadata = function handleLoadedMetadata(e) {
+  totalTime.innerText = formatTime(video.duration);
+  timeline.max = Math.floor(video.duration);
+};
+
+var handleTimeUpdate = function handleTimeUpdate(e) {
+  currentTime.innerText = formatTime(video.currentTime);
+  timeline.value = Math.floor(video.currentTime);
+};
+
+var handleTimeChange = function handleTimeChange(e) {
+  var value = e.target.value;
+  video.currentTime = value;
+};
 
 playBtn.addEventListener("click", handlePlayClick);
-muteBtn.addEventListener("click", handleMute);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
+muteBtn.addEventListener("click", handleMuteClick);
+volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
+timeline.addEventListener("input", handleTimeChange);
--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,6 +4,7 @@ const muteBtn = document.getElementById("mute");
 const volumeRange = document.getElementById("volume");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -48,10 +49,19 @@ const formatTime = (seconds) => new Date(seconds * 1000).toISOString().substr(14
 
 const handleLoadedMetadata = (e) => {
   totalTime.innerText = formatTime(video.duration);
+  timeline.max = Math.floor(video.duration);
 }
 
 const handleTimeUpdate = (e) => {
   currentTime.innerText = formatTime(video.currentTime);
+  timeline.value = Math.floor(video.currentTime);
+}
+
+const handleTimeChange = (e) => {
+  const {
+    target: { value },
+  } = e;
+  video.currentTime = value;
 }
 
 playBtn.addEventListener("click", handlePlayClick);
@@ -59,3 +69,4 @@ muteBtn.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("loadedmetadata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
+timeline.addEventListener("input", handleTimeChange);
