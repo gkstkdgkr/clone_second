351da41fd327e459dff5f0b1494eb4ecb0912395
gkstkdgkr <gkstkdgkr12@gmail.com>
2023-08-19 23:00:01 +0900

    study 230819
    recoding video

--- src/client/js/dist/recoder.dev.js
+++ src/client/js/dist/recoder.dev.js
@@ -1,23 +1,61 @@
 "use strict";
 
 var startBtn = document.getElementById('startBtn');
+var video = document.getElementById('preview');
+var stream;
+var recoder;
+var videoFile;
+
+var handleDownload = function handleDownload() {
+  var a = document.createElement('a');
+  a.href = videoFile;
+  a.download = 'my recoding.mp4';
+  document.body.appendChild(a);
+  a.click();
+};
+
+var handleStop = function handleStop() {
+  startBtn.innerText = 'Start Recording';
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+};
 
 var handleStart = function handleStart() {
-  var stream;
-  return regeneratorRuntime.async(function handleStart$(_context) {
+  startBtn.innerText = 'Stop Recording';
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  recoder = new MediaRecorder(stream);
+
+  recoder.ondataavailable = function (e) {
+    videoFile = URL.createObjectURL(e.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.play();
+  };
+
+  recoder.start();
+  setTimeout(function () {
+    recoder.stop();
+  }, 10000);
+};
+
+var init = function init() {
+  return regeneratorRuntime.async(function init$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
         case 0:
           _context.next = 2;
           return regeneratorRuntime.awrap(navigator.mediaDevices.getUserMedia({
-            audio: true,
+            audio: false,
             video: true
           }));
 
         case 2:
           stream = _context.sent;
+          // video.srcObject = stream;
+          video.play();
 
-        case 3:
+        case 4:
         case "end":
           return _context.stop();
       }
@@ -25,4 +63,5 @@ var handleStart = function handleStart() {
   });
 };
 
+init();
 startBtn.addEventListener('click', handleStart);
--- src/client/js/recoder.js
+++ src/client/js/recoder.js
@@ -1,8 +1,50 @@
-
 const startBtn = document.getElementById('startBtn');
+const video = document.getElementById('preview');
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+let stream;
+let recoder;
+let videoFile;
+
+const handleDownload = () => {
+  const a = document.createElement('a');
+  a.href = videoFile;
+  a.download = 'my recoding.mp4';
+  document.body.appendChild(a);
+  a.click();
+
+
+}
+
+
+const handleStop = () => {
+  startBtn.innerText = 'Start Recording';
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+}
+
+const handleStart = () => {
+  startBtn.innerText = 'Stop Recording';
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+
+  recoder = new MediaRecorder(stream, { mimeType: 'video/mp4');
+  recoder.ondataavailable = (e) => {
+    videoFile = URL.createObjectURL(e.data)
+    video.srcObject = null
+    video.src = videoFile
+    video.play()
+  }
+  recoder.start();
+  setTimeout(() =>{
+    recoder.stop();
+  },10000);
+}
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: true });
+  // video.srcObject = stream;
+  video.play();
 };
+init()
 
 startBtn.addEventListener('click', handleStart);
