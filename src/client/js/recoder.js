94d0bc271d7fe2c6c97425074bb451a6e248232e
gkstkdgkr <gkstkdgkr12@gmail.com>
2023-08-22 02:28:15 +0900

    study 230822
    fixed recoder

--- src/client/js/recoder.js
+++ src/client/js/recoder.js
@@ -7,28 +7,34 @@ let stream;
 let recoder;
 let videoFile;
 
+const files ={
+  input:"recoding.webm",
+  output:"output.mp4",
+  thumb:"thumbnail.jpg"
+};
+
 const handleDownload = async () => {
   const ffmpeg = createFFmgeg({ log: true });
   await ffmpeg.load();
 
   // ffmpeg 공간의 가상의 파일 생성
-  ffmpeg.FS("writeFile", "recoding.webm", await fetchFile(videoFile));
+  ffmpeg.FS("writeFile", "files.input", await fetchFile(videoFile));
 
-  await ffmpeg.run("-i", "recoding.webm", "-r", "60", "output.mp4");
-  // input형식 recoding.webm파일을 -r , 60 60프레임으로 인코딩 결과값 "output.mp4"
+  await ffmpeg.run("-i", "files.input", "-r", "60", "files.output");
+  // input형식 files.input파일을 -r , 60 60프레임으로 인코딩 결과값 "files.output"
   await ffmpeg.run(
     "-i",
-    "recoding.webm",
+    "files.input",
     "-ss",
     "00:00:01",
     "-frames:v",
     "1",
-    "thumbnail.jpg"
-  );
+    "files.thumb"
+  )
 
-  const mp4File = ffmpeg.FS("readFile", "output.mp4");
-  const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
-
+  const mp4File = ffmpeg.FS("readFile", "files.output");
+  const thumbFile = ffmpeg.FS("readFile", "files.thumb");
+    // new Blob([바이너리 데이터]) => 파일같은 객체 생성
   const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
   const thumbBlob= new Blob([thumbFile.buffer], { type: "image/jpg" });
 
@@ -46,6 +52,14 @@ const handleDownload = async () => {
   thumbaA.download = "MyThumbnail.jpg";
   document.body.appendChild(thumbaA);
   thumbaA.click();
+
+  ffmpeg.FS("unlink", "files.output");
+  ffmpeg.FS("unlink", "files.thumb");
+  ffmpeg.FS("unlink", "files.input");
+
+  URL.revokeObjectURL(mp4Url);
+  URL.revokeObjectURL(thumbUrl);
+  URL.revokeObjectURL(videoFile);
 };
 
 const handleStop = () => {
