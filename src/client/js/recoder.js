daa6ebb624e71174f178b4ed51435bbef565ba33
gkstkdgkr <gkstkdgkr12@gmail.com>
2023-08-22 03:39:12 +0900

    study 230822
    ERROR

--- src/client/js/recoder.js
+++ src/client/js/recoder.js
@@ -1,91 +1,97 @@
-import { createFFmgeg, fetchFile } from "@ffmpeg/ffmpeg";
-import { async } from "regenerator-runtime";
-const startBtn = document.getElementById("startBtn");
+import { FFmpeg } from '@ffmpeg/ffmpeg'
+import { fetchFile } from '@ffmpeg/util'
+const actionBtn = document.getElementById("actionBtn");
 const video = document.getElementById("preview");
 
 let stream;
-let recoder;
+let recorder;
 let videoFile;
 
-const files ={
-  input:"recoding.webm",
-  output:"output.mp4",
-  thumb:"thumbnail.jpg"
+const files = {
+  input: "recording.webm",
+  output: "output.mp4",
+  thumb: "thumbnail.jpg",
+};
+
+const downloadFile = (fileUrl, fileName) => {
+  const a = document.createElement("a");
+  a.href = fileUrl;
+  a.download = fileName;
+  document.body.appendChild(a);
+  a.click();
 };
 
 const handleDownload = async () => {
-  const ffmpeg = createFFmgeg({ log: true });
-  await ffmpeg.load();
+  actionBtn.removeEventListener("click", handleDownload);
+
+  actionBtn.innerText = "Transcoding...";
+
+  actionBtn.disabled = true;
+
+  const ffmpeg = new FFmpeg()
+	await ffmpeg.load()
 
   // ffmpeg 공간의 가상의 파일 생성
-  ffmpeg.FS("writeFile", "files.input", await fetchFile(videoFile));
+	await ffmpeg.writeFile(files.input, await fetchFile(videoFile));
 
-  await ffmpeg.run("-i", "files.input", "-r", "60", "files.output");
+  await ffmpeg.exec(["-i", files.input, "-r", "60", files.output]);
   // input형식 files.input파일을 -r , 60 60프레임으로 인코딩 결과값 "files.output"
-  await ffmpeg.run(
-    "-i",
-    "files.input",
+  await ffmpeg.exec(
+    ["-i",
+    files.input,
     "-ss",
     "00:00:01",
     "-frames:v",
     "1",
-    "files.thumb"
-  )
+     files.thumb]
+  );
 
-  const mp4File = ffmpeg.FS("readFile", "files.output");
-  const thumbFile = ffmpeg.FS("readFile", "files.thumb");
-    // new Blob([바이너리 데이터]) => 파일같은 객체 생성
+  const mp4File = ffmpeg.FS("readFile", files.output);
+  const thumbFile = ffmpeg.FS("readFile", files.thumb);
+  // new Blob([바이너리 데이터]) => 파일같은 객체 생성
   const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
-  const thumbBlob= new Blob([thumbFile.buffer], { type: "image/jpg" });
+  const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
 
   const mp4Url = URL.createObjectURL(mp4Blob);
   const thumbUrl = URL.createObjectURL(thumbBlob);
 
-  const a = document.createElement("a");
-  a.href = mp4Url;
-  a.download = "MyRecoding.mp4";
-  document.body.appendChild(a);
-  a.click();
+  downloadFile(mp4Url, "MyRecoding.mp4");
+  downloadFile(thumbUrl, "MyThumbnail.jpg");
 
-  const thumbaA = document.createElement("a");
-  thumbaA.href = mp4Url;
-  thumbaA.download = "MyThumbnail.jpg";
-  document.body.appendChild(thumbaA);
-  thumbaA.click();
-
-  ffmpeg.FS("unlink", "files.output");
-  ffmpeg.FS("unlink", "files.thumb");
-  ffmpeg.FS("unlink", "files.input");
+  ffmpeg.FS("unlink", files.output);
+  ffmpeg.FS("unlink", files.thumb);
+  ffmpeg.FS("unlink", files.input);
 
   URL.revokeObjectURL(mp4Url);
   URL.revokeObjectURL(thumbUrl);
   URL.revokeObjectURL(videoFile);
+
+  actionBtn.disabled= false;
+  actionBtn.innerText = "Record Again";
+  actionBtn.addEventListener("click", handleStart);
 };
 
 const handleStop = () => {
-  startBtn.innerText = "Download Recording";
-  startBtn.removeEventListener("click", handleStop);
-  startBtn.addEventListener("click", handleStart);
-  recoder.stop();
+  actionBtn.innerText = "Download Recording";
+  actionBtn.removeEventListener("click", handleStop);
+  actionBtn.addEventListener("click", handleDownload);
+  recorder.stop();
 };
 
 const handleStart = () => {
-  startBtn.innerText = "Stop Recording";
-  startBtn.removeEventListener("click", handleStart);
-  startBtn.addEventListener("click", handleStop);
+  actionBtn.innerText = "Stop Recording";
+  actionBtn.removeEventListener("click", handleStart);
+  actionBtn.addEventListener("click", handleStop);
 
-  recoder = new MediaRecorder(stream, { mimeType: "video/webm" });
-  recoder.ondataavailable = (e) => {
+  recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
+  recorder.ondataavailable = (e) => {
     videoFile = URL.createObjectURL(e.data);
     video.srcObject = null;
     video.src = videoFile;
     video.loop = true;
     video.play();
   };
-  recoder.start();
-  setTimeout(() => {
-    recoder.stop();
-  }, 10000);
+  recorder.start();
 };
 
 const init = async () => {
@@ -98,4 +104,4 @@ const init = async () => {
 };
 init();
 
-startBtn.addEventListener("click", handleStart);
+actionBtn.addEventListener("click", handleStart);
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -8,9 +8,7 @@ import {
   getUpload,
   postUpload,
 } from "../controllers/videoController";
-import {
-  protectorMiddleware, videoUpload
-} from "../middlewares";
+import { protectorMiddleware, videoUpload } from "../middlewares";
 
 const videoRouter = express.Router();
 
@@ -18,12 +16,29 @@ const videoRouter = express.Router();
 // :id가 위에있으면 /upload는 인식 못함
 // 리퀘스트는 제일 위에있는것부터 순차적으로 확인함
 
-// :id(//d+) id를 불러오는데 숫자만 받아옴 
+// :id(//d+) id를 불러오는데 숫자만 받아옴
 // :id는 express에게 id라는 변수를 받아오라는 표시
 
 // 지금 현재 받아오는 id는 24비트 16진수([0-9a-f]{24})
 videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
-videoRouter.route("/:id([0-9a-f]{24})/delete").all(protectorMiddleware).get(deleteVideo);
-videoRouter.route("/upload").all(protectorMiddleware).get(getUpload).post(videoUpload.single("video"), postUpload);
+videoRouter
+  .route("/:id([0-9a-f]{24})/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(postEdit);
+videoRouter
+  .route("/:id([0-9a-f]{24})/delete")
+  .all(protectorMiddleware)
+  .get(deleteVideo);
+videoRouter
+  .route("/upload")
+  .all(protectorMiddleware)
+  .get(getUpload)
+  .post(
+    videoUpload.fields([
+      { name: "video", maxCount: 1 },
+      { name: "thumb", maxCount: 1 },
+    ]),
+    postUpload
+  );
 export default videoRouter;
