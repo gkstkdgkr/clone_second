ea79afa74dca0a07b48309a74daffb243e5bd76e
gkstkdgkr <gkstkdgkr12@gmail.com>
2023-08-23 04:22:29 +0900

    study 230823
    comment box

---
+++ src/client/js/commentSection.js
@@ -0,0 +1,13 @@
+const videoContainer = document.getElementById('videoContainer');
+const form = document.getElementById('commentform');
+const textarea = form.querySelector('textarea');
+const btn = form.querySelector('button');
+
+const handleSubmit = (e) =>{
+  e.preventDefault();
+  const text = textarea.value;
+  const video = videoContainer.dataset.id;
+  
+}
+
+form.addEventListener('submit', handleSubmit);
